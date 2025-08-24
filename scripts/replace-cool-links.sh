4e254cbd7e3e0400f27e7a5080f694aa81cec27c
GitGudBruhh <gitgudbruhh@users.noreply.github.com>
2025-08-24 15:08:50 +0530

    Added Videos section and updated with new links

---
+++ scripts/replace-cool-links.sh
@@ -0,0 +1,1 @@
+python scripts/md2html-replacer.py --md-file src/pages/CoolLinks.md --output-file src/pages/cool-links.html --template-file scripts/cool-links-template.html --backup-folder ignore/md2html-backups/
